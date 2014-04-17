7ee0fde816f1cead321633b84bb9c72afbc8c9a7
ShubhamRathi <srrathi@hotmail.com>
2014-04-17 22:55:12 +0530

    Pushing the Final Code
    Trigger+Video Call

---
+++ Audio Reactive Trigger/js/freq.js
@@ -0,0 +1,162 @@
+var audioContext = new AudioContext();
+var isPlaying = false;
+var sourceNode = null;
+var analyser = null;
+var theBuffer = null;
+var WIDTH=300;
+var CENTER=150;
+var HEIGHT=42;
+var confidence = 0;
+var currentPitch = 0;
+
+window.onload = function() {
+	
+		theBuffer = null;
+
+	  	var reader = new FileReader();
+	  	reader.onload = function (event) {
+	  		audioContext.decodeAudioData( event.target.result, function(buffer) {
+	    		theBuffer = buffer;
+	  		}, function(){alert("error loading!");} ); 
+
+	  	};
+	  	reader.onerror = function (event) {
+	  		alert("Error: " + reader.error );
+		};
+	  	reader.readAsArrayBuffer(e.dataTransfer.files[0]);
+	  	return false;
+	};
+
+
+function error() {
+    alert('Stream generation failed.');
+}
+
+function getUserMedia(dictionary, callback) {
+    try {
+        navigator.getUserMedia = 
+        	navigator.getUserMedia ||
+        	navigator.webkitGetUserMedia ||
+        	navigator.mozGetUserMedia;
+        navigator.getUserMedia(dictionary, callback, error);
+    } catch (e) {
+        alert('getUserMedia threw exception :' + e);
+    }
+}
+
+function gotStream(stream) {
+    // Create an AudioNode from the stream.
+    var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+    // Connect it to the destination.
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    mediaStreamSource.connect( analyser );
+    updatePitch();
+}
+
+function toggleLiveInput() {
+    getUserMedia({audio:true}, gotStream);
+}
+
+function togglePlayback() {
+    var now = audioContext.currentTime;
+
+    if (isPlaying) {
+        //stop playing and return
+        sourceNode.stop( now );
+        sourceNode = null;
+        analyser = null;
+        isPlaying = false;
+		if (!window.cancelAnimationFrame)
+			window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
+        window.cancelAnimationFrame( rafID );
+        return "start";
+    }
+
+    sourceNode = audioContext.createBufferSource();
+    sourceNode.buffer = theBuffer;
+    sourceNode.loop = true;
+
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    sourceNode.connect( analyser );
+    analyser.connect( audioContext.destination );
+    sourceNode.start( now );
+    isPlaying = true;
+    isLiveInput = false; // Why should this be False?
+    updatePitch();
+
+    return "stop"; // Why should this return False?
+}
+
+var rafID = null;
+var tracks = null;
+var buflen = 2048;
+var buf = new Uint8Array( buflen );
+var MINVAL = 134;  // 128 == zero.  MINVAL is the "minimum detected signal" level.
+
+
+function autoCorrelate( buf, sampleRate ) {
+	var MIN_SAMPLES = 4;	// corresponds to an 11kHz signal
+	var MAX_SAMPLES = 1000; // corresponds to a 44Hz signal
+	var SIZE = 1000;
+	var best_offset = -1;
+	var best_correlation = 0;
+	var rms = 0;
+
+	confidence = 0;
+	currentPitch = 0;
+
+	if (buf.length < (SIZE + MAX_SAMPLES - MIN_SAMPLES))
+		return;  // Not enough data
+
+	for (var i=0;i<SIZE;i++) {
+		var val = (buf[i] - 128)/128;
+		rms += val*val;
+	}
+	rms = Math.sqrt(rms/SIZE);
+
+	for (var offset = MIN_SAMPLES; offset <= MAX_SAMPLES; offset++) {
+		var correlation = 0;
+
+		for (var i=0; i<SIZE; i++) {
+			correlation += Math.abs(((buf[i] - 128)/128)-((buf[i+offset] - 128)/128));
+		}
+		correlation = 1 - (correlation/SIZE);
+		if (correlation > best_correlation) {
+			best_correlation = correlation;
+			best_offset = offset;
+		}
+	}
+	if ((rms>0.01)&&(best_correlation > 0.01)) {
+		confidence = best_correlation * rms * 10000;
+		currentPitch = sampleRate/best_offset;
+		if(currentPitch<3500 && currentPitch>1500) // Checks if Whistle
+		{ 
+			console.log("Trigger at " + currentPitch )
+		  	run();
+		}
+	}
+}
+
+function updatePitch( time ) {
+	var cycles = new Array;
+	analyser.getByteTimeDomainData( buf );
+	autoCorrelate( buf, audioContext.sampleRate );
+	rafID = window.requestAnimationFrame( updatePitch );
+}
+
+function run() { // Trigger the Image Animation
+    var frames = document.getElementById("animation").children;
+    var frameCount = frames.length;
+    var i = 0;
+    var n = setInterval(function () { 
+        if(i==2)
+         {clearInterval(n);
+	   window.location="https://googledrive.com/host/0B0byAC8kZZPhQnZRSGFDWlliTk0/";
+}
+
+        frames[i % frameCount].style.display = "none";
+        frames[++i % frameCount].style.display = "block";
+    }, 1000);}
---
+++ Peer-To-Peer Call/blackline.js
@@ -0,0 +1,11 @@
+function myfunction1()
+{
+document.getElementById("mytable").style.border="thin solid black";
+document.getElementById("mytable").style.backgroundColor="#900000";
+}
+function myfunction2()
+{
+document.getElementById("mytable").style.border="";
+document.getElementById("mytable").style.backgroundColor="";
+}
+
